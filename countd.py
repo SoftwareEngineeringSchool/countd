7d266a1fd4364b145b90ca8329436dbe97409073
krintalina <krintalina@gmail.com>
2015-08-11 16:18:43 +0300

    Version 01

--- countd.py
+++ countd.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python
+import sys
+from datetime import datetime
+arg1=sys.argv[1]
+if len(sys.argv)==2:
+	currentt=datetime.now()
+	arg2=currentt.strftime("%d/%m/%Y")
+else:
+	arg2=sys.argv[2]
+def countDays(date1=arg1,date2=arg2):
+	myFormat="%d/%m/%Y"
+	d1=datetime.strptime(date1, myFormat)
+	d2=datetime.strptime(date2, myFormat)
+	nrDays=abs(d2-d1)
+	print 'Between %s and %s are: %d days'%(date1,date2,nrDays.days)
+countDays()
